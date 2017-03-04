fe3ebc912a846842208be2a2fe4c2dc6eb892982
sprusr <scott@spru.sr>
2017-03-04 21:50:04 +0100

    Implement and fix s3 stuff

--- scripts/aws.js
+++ scripts/aws.js
@@ -3,10 +3,10 @@ const conf = require('../config.js')
 const AWS = require('aws-sdk')
 const crypto = require('crypto')
 
-AWS.config.loadFromPath(__dirname + '/../aws.json');
+AWS.config.loadFromPath(__dirname + '/../aws.json')
 
 // Create S3 service object
-var s3 = new AWS.S3( { params: {Bucket: 'hackupc'} });
+var s3 = new AWS.S3( { params: {Bucket: 'hackupc'} })
 
 function checksum (str) {
     return crypto
@@ -17,15 +17,13 @@ function checksum (str) {
 
 module.exports = {
   //@Param file: Buffer
-  upload: function(file){
-    let data = {Key: checksum(file), Body: file};
-    return new Promise( (resolve, reject)=>{
-      s3.putObject(data, function(err, data){
+  upload: function(file) {
+    let data = {Key: checksum(file), Body: file}
+    return new Promise((resolve, reject) => {
+      s3.putObject(data, (err, result) => {
         if (err) return reject(err)
         resolve('https://hackupc.s3.amazonaws.com/' + data.Key)
-      });
+      })
     })
-
   }
-
 }
