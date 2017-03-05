52c3aafcfd2026b0906ba89160c4ff2ecb94cd3f
Pandelis Zembashis <info@pandelis.me>
2017-03-05 05:43:18 +0100

    working and valid mime types

--- scripts/aws.js
+++ scripts/aws.js
@@ -20,6 +20,7 @@ module.exports = {
   upload: function(file) {
     let ext = fileType(file)
     let data = {Key: checksum(file) + '.' + ext.ext, Body: file, ContentType: ext.mime, ACL: 'public-read'}
+    console.log(data)
     return new Promise((resolve, reject) => {
       s3.putObject(data, (err, result) => {
         if (err) return reject(err)
